972555956c4406de8e1b0bb6b822281af37c3c95
yagi17 <nafissadik785@gmail.com>
2024-06-02 18:06:56 +0600

    added users api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express');
-
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
@@ -12,7 +11,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bvaa0x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -29,6 +27,45 @@ const dbConnect = async () => {
     }
 };
 dbConnect();
+
+
+// DB COLLECTION
+const userCollection = client.db('EduPortal').collection('users')
+const classCollection = client.db('EduPortal').collection('classes')
+const reviewCollection = client.db('EduPortal').collection('reviews')
+
+// User APIs
+app.get('/users', async (req, res) => {
+    const result = await userCollection.find().toArray()
+})
+
+app.post('/users', async (req, res) => {
+    const user = req.body
+
+    const result = await userCollection.insertOne(user)
+})
+
+// Class APIs
+app.get('/classes', async (req, res) => {
+    const result = await classCollection.find().toArray()
+    res.send(result)
+})
+
+app.get('/classes/:id', async (req, res) => {
+    const id = req.params.id
+    const query = { _id: new ObjectId(id) }
+    const result = await classCollection.findOne(query)
+    res.send(result)
+})
+
+// Reviews APIs
+app.get('/reviews', async (req, res) => {
+    const result = await reviewCollection.find().toArray()
+    res.send(result)
+})
+
+
+
 
 
 // ---------------- //
